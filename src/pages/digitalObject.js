63a3b8ce150157b6a31254431860129362854cb8
enderu <enderu@gmail.com>
2022-03-28 12:41:29 -0500

    mostly whitespace, logging test

--- src/pages/digitalObject.js
+++ src/pages/digitalObject.js
@@ -1,10 +1,10 @@
-import React, {useEffect, useState, useContext } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import clsx from 'clsx'
 
 // ------------------------
 // Material
 // ------------------------
-import {  Accordion, AccordionSummary, AccordionDetails, Box, Button, Checkbox, Grid, FormControl, IconButton, InputLabel, InputBase, List, ListItem, ListItemSecondaryAction, ListItemText, Select, Typography, Chip } from '@material-ui/core/'
+import { Accordion, AccordionSummary, AccordionDetails, Box, Button, Checkbox, Grid, FormControl, IconButton, InputLabel, InputBase, List, ListItem, ListItemSecondaryAction, ListItemText, Select, Typography, Chip } from '@material-ui/core/'
 import { Alert } from '@material-ui/lab'
 import GetAppIcon from '@material-ui/icons/GetApp'
 
@@ -36,26 +36,27 @@ import { AppBreadcrumbs } from '../parts/appBreadcrumbs'
 const dataTypes = ['project', 'dataset', 'file']
 
 export const DigitalObject = (props) => {
-    
+
     const classes = useStyles()
     const [checked, setChecked] = useState([]) // for one filter (array of...)
     const [expanded, setExpanded] = useState(0);
 
-    const [ searchState, searchDispatch ] = useContext(SearchContext)
-    const [ filterState, filterDispatch ] = useContext(FilterContext)
+    const [searchState, searchDispatch] = useContext(SearchContext)
+    const [filterState, filterDispatch] = useContext(FilterContext)
 
-    const [ fetchState, fetchData ] = useFetchAPI()
-    
+    const [fetchState, fetchData] = useFetchAPI()
+
     const { page, env, match } = props
-       
-    const [ content, setContent ] = useState(null);
-  
+
+    const [content, setContent] = useState(null);
+
     const component = (page.path).split('/').filter(q => q != "")[0]
     const id = match.params.id
     const query = `${component}/${id}`
-    
+
     const handleDownload = (url) => {
-        
+        console.log("alextest")
+
         console.log(url)
     }
 
@@ -63,14 +64,14 @@ export const DigitalObject = (props) => {
     // Set digital object content:
     // ------------------------------
     useEffect(() => {
-        if(!isObjEmpty(fetchState.data)){
+        if (!isObjEmpty(fetchState.data)) {
             console.log(fetchState)
-            if(fetchState.data.results.bindings.length > 0){
+            if (fetchState.data.results.bindings.length > 0) {
                 let currentResults = fetchState.data.results.bindings
                 let updatedContent = {}
-                
+
                 currentResults.forEach(option => {
-                    let currentKey = (option.p.value).replace('https://schema.org/','')
+                    let currentKey = (option.p.value).replace('https://schema.org/', '')
                     updatedContent[currentKey] = {
                         "schema": option.p.value,
                         "value": option.o.value,
@@ -80,42 +81,42 @@ export const DigitalObject = (props) => {
                 // console.log(updatedContent)
                 setContent(updatedContent)
             }
-        } else{
+        } else {
             setContent(null)
         }
     }, [fetchState])
 
     useEffect(() => {
-        
+
         (async () => {
-            if(match.params.id && !isEmpty(match.params.id)){
-                
-                const {url, context} = digitalObjectSearch(env.graph, id)
+            if (match.params.id && !isEmpty(match.params.id)) {
+
+                const { url, context } = digitalObjectSearch(env.graph, id)
                 console.log(url, context)
-                
+
                 // await getContent(url, context)
                 await fetchData(url, context)
-                
+
             } else {
                 setContent({})
             }
-        
+
         })()
-    
+
     }, [])
 
-    return(
-        <>        
+    return (
+        <>
             <AppBreadcrumbs {...props} />
-            <Box 
-                bgcolor={'inherit'} 
+            <Box
+                bgcolor={'inherit'}
                 position={'relative'}
-                p={0} 
-                boxShadow={2} 
-                borderRadius={4} 
-                // className={clsx(classes.boxSearchResults)}
-            >   
-                { (!isObjEmpty(content)) ?
+                p={0}
+                boxShadow={2}
+                borderRadius={4}
+            // className={clsx(classes.boxSearchResults)}
+            >
+                {(!isObjEmpty(content)) ?
                     <>
                         {console.log(content)}
                         <Box p={4} justifyContent={'center'} alignItems={'center'} bgcolor={'grey.200'}>
@@ -125,7 +126,7 @@ export const DigitalObject = (props) => {
                                 </Typography>
                             </Box>
                             <Box py={1}>
-                                <Chip 
+                                <Chip
                                     variant={'outlined'}
                                     color={'primary'}
                                     label={content.encodingFormat.value} />
@@ -139,32 +140,35 @@ export const DigitalObject = (props) => {
 
                             <Box my={1}>
                                 <Button
-                                    color={'primary'} 
-                                    variant={'contained'} 
-                                    download={content.url.name}
+                                    color={'primary'}
+                                    variant={'contained'}
+                                    download={content.name.value}
                                     // component={'a'}
                                     onClick={handleDownload(content.url.value)}
+                                // onClick={() => { console.log(content); }}
                                 >
                                     Download
                                 </Button>
 
+                                <a href={content.url.value} download>Download File</a>
+
                             </Box>
                         </Box>
-                        
+
                         <Box >
                             <List component="ul" aria-label="download list">
-                                
+
                                 {(!isObjEmpty(content.dateCreated)) &&
-                                    <ListItem key={`list-item-${content.dateCreated.key}`} divider > 
-                                        <ListItemText 
-                                            primary={content.dateCreated.value} 
+                                    <ListItem key={`list-item-${content.dateCreated.key}`} divider >
+                                        <ListItemText
+                                            primary={content.dateCreated.value}
                                             secondary={content.dateCreated.key}
                                         />
                                     </ListItem>
                                 }
                                 {(!isObjEmpty(content.isRelatedTo)) &&
-                                    <ListItem key={`list-item-${content.isRelatedTo.key}`} divider > 
-                                        <ListItemText 
+                                    <ListItem key={`list-item-${content.isRelatedTo.key}`} divider >
+                                        <ListItemText
                                             primary={
                                                 <a href={`/res/${content.isRelatedTo.value}`} title='project related to'>
                                                     {content.isRelatedTo.value}
@@ -175,31 +179,31 @@ export const DigitalObject = (props) => {
                                     </ListItem>
                                 }
                                 {(!isObjEmpty(content.identifier)) &&
-                                    <ListItem key={`list-item-${content.identifier.key}`} divider > 
-                                        <ListItemText 
-                                            primary={content.identifier.value} 
+                                    <ListItem key={`list-item-${content.identifier.key}`} divider >
+                                        <ListItemText
+                                            primary={content.identifier.value}
                                             secondary={content.identifier.key}
                                         />
                                     </ListItem>
                                 }
                                 {(!isObjEmpty(content.license)) &&
-                                    <ListItem key={`list-item-${content.license.key}`} divider > 
-                                        <ListItemText 
+                                    <ListItem key={`list-item-${content.license.key}`} divider >
+                                        <ListItemText
                                             primary={
                                                 <a href={content.license.value} title='license information'>
                                                     {content.license.value}
                                                 </a>
                                             }
-                                            secondary={content.license.key} 
+                                            secondary={content.license.key}
                                         />
                                     </ListItem>
                                 }
-                                
+
                             </List>
                         </Box>
                     </>
-                    
-                : 
+
+                    :
                     <Alert severity={'error'}>
                         'No file available for this ID.'
                     </Alert>
@@ -208,5 +212,5 @@ export const DigitalObject = (props) => {
         </>
     )
 }
-  
-  export default DigitalObject
+
+export default DigitalObject
